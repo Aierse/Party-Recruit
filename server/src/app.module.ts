567c9294e8e3e7ecce669c90e8f5295c6313414c
Aierse <dbsgur2008@gmail.com>
2022-12-12 23:59:00 +0900

    create - user API

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,10 +5,12 @@ import { ProfileController } from './profile/profile.controller';
 import { ProfileService } from './profile/profile.service';
 import { ProfileModule } from './profile/profile.module';
 import { ProjectModule } from './project/project.module';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [ProfileModule, ProjectModule],
-  controllers: [AppController, ProfileController],
-  providers: [AppService, ProfileService],
+  controllers: [AppController, ProfileController, UserController],
+  providers: [AppService, ProfileService, UserService],
 })
 export class AppModule {}
